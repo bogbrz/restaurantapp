cfd6ad154415a2616bf5d0c164b0299a29c5d504
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2023-09-27 12:58:13 +0200

    Organise imports

--- lib/app/features/tables_page_content/pages/tablecontentpage/cubit/tablecontent_cubit.dart
+++ lib/app/features/tables_page_content/pages/tablecontentpage/cubit/tablecontent_cubit.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 import 'package:restaurantapp/models/tablepagemodel.dart';
 import 'package:restaurantapp/repositories/table_repository.dart';
