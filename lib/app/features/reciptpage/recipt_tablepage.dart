51710597f3cf2d8afcc4b05887c5410778ba237a
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2023-09-29 14:15:56 +0200

    Add buttons to reciptpage and create recipttablepage

---
+++ lib/app/features/reciptpage/recipt_tablepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurantapp/app/features/tables_page_content/cubit/table_page_cubit.dart';
+import 'package:restaurantapp/repositories/table_repository.dart';
+
+class ReciptTablePage extends StatelessWidget {
+  const ReciptTablePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('dupsko'));
+  }
+}
--- lib/app/features/reciptpage/reciptpage.dart
+++ lib/app/features/reciptpage/reciptpage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurantapp/app/features/reciptpage/recipt_tablepage.dart';
+import 'package:restaurantapp/app/features/tables_page_content/cubit/table_page_cubit.dart';
+import 'package:restaurantapp/repositories/table_repository.dart';
 //a
 
 class ReciptPage extends StatelessWidget {
@@ -8,8 +12,62 @@ class ReciptPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("recipt")),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recipts'),
+        backgroundColor: Colors.orange,
+      ),
+      body: BlocProvider(
+        create: (context) => TablePageCubit(TableRepository())..start(),
+        child: BlocBuilder<TablePageCubit, TablePageState>(
+          builder: (context, state) {
+            final tableModels = state.tables;
+            return ListView(
+              children: [
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        border: Border.all(color: Colors.black, width: 4),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        "Choose the table",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ],
+                ),
+                for (final tableModel in tableModels) ...[
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (_) => const ReciptTablePage())),
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            alignment: Alignment.center,
+                            color: Colors.orange,
+                            child: Text(tableModel.number),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ]
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
