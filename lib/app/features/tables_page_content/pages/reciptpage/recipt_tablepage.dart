3d25d2b235150fdc4198199f4666ca5d31f31230
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2023-10-01 13:59:57 +0200

    Add containers to reciptpage

--- lib/app/features/tables_page_content/pages/reciptpage/recipt_tablepage.dart
+++ lib/app/features/tables_page_content/pages/reciptpage/recipt_tablepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurantapp/app/features/tables_page_content/pages/reciptpage/cubit/recipt_page_cubit.dart';
+import 'package:restaurantapp/app/features/tables_page_content/pages/tablecontentpage/cubit/tablecontent_cubit.dart';
 import 'package:restaurantapp/repositories/table_repository.dart';
 
 class ReciptTablePage extends StatelessWidget {
@@ -17,35 +18,136 @@ class ReciptTablePage extends StatelessWidget {
           title: Text("table number $tableModel"),
           backgroundColor: Colors.orange,
         ),
-        body: BlocProvider(
-          create: (context) => ReciptPageCubit(TableRepository())..start(),
+        body: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => ReciptPageCubit(TableRepository())..start(),
+            ),
+            BlocProvider(
+              create: (context) =>
+                  TablecontentCubit(TableRepository())..start(),
+            ),
+          ],
           child: BlocBuilder<ReciptPageCubit, ReciptPageState>(
             builder: (context, state) {
               final recipts = state.recipts;
+
               return ListView(children: [
                 for (final recipt in recipts) ...[
                   if (recipt.number == tableModel)
-                    Column(
+                    Row(
                       children: [
-                        Container(
-                          color: Colors.orange,
-                          child: Text(recipt.v1.toString()),
-                        ),
-                        Container(
-                          color: Colors.orange,
-                          child: Text(recipt.v2.toString()),
-                        ),
-                        Container(
-                          color: Colors.orange,
-                          child: Text(recipt.v3.toString()),
-                        ),
-                        Container(
-                          color: Colors.orange,
-                          child: Text(recipt.v4.toString()),
-                        ),
-                        Container(
-                          color: Colors.orange,
-                          child: Text(recipt.number.toString()),
+                        BlocBuilder<TablecontentCubit, TablecontentState>(
+                            builder: (context, state) {
+                          if (state.tablePageModels.isEmpty) {
+                            return const Center(
+                              child: Text('no data avalibe'),
+                            );
+                          }
+                          if (state.isLoading) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          }
+                          if (state.errorMessage.isNotEmpty) {
+                            return const Text("something went wrong");
+                          }
+                          final tablePageModels = state.tablePageModels;
+                          return Column(
+                            children: [
+                              for (final tablePageModel in tablePageModels) ...[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(children: [
+                                    Column(
+                                      children: [
+                                        Container(
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Colors.black, width: 2),
+                                            color: Colors.orange,
+                                          ),
+                                          height: 50,
+                                          width: 100,
+                                          alignment: Alignment.center,
+                                          child: Text(tablePageModel.name),
+                                        ),
+                                      ],
+                                    ),
+                                  ]),
+                                ),
+                              ],
+                            ],
+                          );
+                        }),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black,
+                                    width: 2,
+                                  ),
+                                  color: Colors.orange,
+                                ),
+                                height: 50,
+                                width: 50,
+                                alignment: Alignment.center,
+                                child: Text(recipt.v1.toString()),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black,
+                                    width: 2,
+                                  ),
+                                  color: Colors.orange,
+                                ),
+                                height: 50,
+                                width: 50,
+                                alignment: Alignment.center,
+                                child: Text(recipt.v2.toString()),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black,
+                                    width: 2,
+                                  ),
+                                  color: Colors.orange,
+                                ),
+                                height: 50,
+                                width: 50,
+                                alignment: Alignment.center,
+                                child: Text(recipt.v3.toString()),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black,
+                                    width: 2,
+                                  ),
+                                  color: Colors.orange,
+                                ),
+                                height: 50,
+                                width: 50,
+                                alignment: Alignment.center,
+                                child: Text(
+                                  recipt.v4.toString(),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
