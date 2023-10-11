e9d3931daf69b8f1cb0a04facd58a98085ed0b6d
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2023-10-11 22:00:26 +0200

    Implement blocProvider, add Drinks name

--- lib/app/features/bar_page/pages/barman_page.dart
+++ lib/app/features/bar_page/pages/barman_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurantapp/app/features/tables_page_content/pages/tablecontentpage/cubit/tablecontent_cubit.dart';
+import 'package:restaurantapp/repositories/table_repository.dart';
 
 class BarmanPage extends StatefulWidget {
   const BarmanPage({super.key});
@@ -26,61 +29,103 @@ class OrdersPage extends StatelessWidget {
         title: const Text('Incoming orders'),
         backgroundColor: Colors.orange,
       ),
-      body: ListView(children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 50, right: 50, top: 20),
-          child: Container(
-            decoration: const BoxDecoration(color: Colors.orange),
-            child: Column(
-              children: [
-                const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 8, top: 8, right: 8),
-                      child: Text("NUMER STOLIKA"),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 15, right: 15, bottom: 8, top: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: BlocProvider(
+        create: (context) => TablecontentCubit(TableRepository())..start(),
+        child: BlocBuilder<TablecontentCubit, TablecontentState>(
+          builder: (context, state) {
+            final tablePageModels = state.tablePageModels;
+            return ListView(children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50, top: 20),
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.orange),
+                  child: Column(
                     children: [
-                      Column(
+                      const Row(
                         children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 2,
-                                color: Colors.black,
-                              ),
-                            ),
-                            child: const Text('Nazwa drinka'),
+                          Padding(
+                            padding: EdgeInsets.only(left: 8, top: 8, right: 8),
+                            child: Text("NUMER STOLIKA"),
                           ),
                         ],
                       ),
-                      Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 2,
-                                color: Colors.black,
-                              ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 15, right: 15, bottom: 8, top: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  alignment: Alignment.center,
+                                  width: 100,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.amber,
+                                    border: Border.all(
+                                      width: 2,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "Drinks",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                for (final tablePageModel
+                                    in tablePageModels) ...[
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      width: 100,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        color: Colors.amber,
+                                        border: Border.all(
+                                          width: 2,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      child: Text(
+                                        tablePageModel.name,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                ]
+                              ],
                             ),
-                            child: const Text("ilość"),
-                          ),
-                        ],
+                            Column(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      width: 2,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  child: const Text("ilość"),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
                       )
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
+                ),
+              ),
+            ]);
+          },
         ),
-      ]),
+      ),
     );
   }
 }
