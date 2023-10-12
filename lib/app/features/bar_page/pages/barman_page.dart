988b1b6789ddb38353d1c8971f255e5f5feb010a
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2023-10-12 10:47:31 +0200

    Implement order amount column

--- lib/app/features/bar_page/pages/barman_page.dart
+++ lib/app/features/bar_page/pages/barman_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurantapp/app/features/tables_page_content/pages/reciptpage/cubit/recipt_page_cubit.dart';
 import 'package:restaurantapp/app/features/tables_page_content/pages/tablecontentpage/cubit/tablecontent_cubit.dart';
 import 'package:restaurantapp/repositories/table_repository.dart';
 
@@ -11,16 +12,11 @@ class BarmanPage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<BarmanPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const OrdersPage();
-  }
-}
-
-class OrdersPage extends StatelessWidget {
-  const OrdersPage({
-    super.key,
-  });
+  var rv1 = 0;
+  var rv2 = 0;
+  var rv3 = 0;
+  var rv4 = 0;
+  var tnumber = '';
 
   @override
   Widget build(BuildContext context) {
@@ -29,56 +25,125 @@ class OrdersPage extends StatelessWidget {
         title: const Text('Incoming orders'),
         backgroundColor: Colors.orange,
       ),
-      body: BlocProvider(
-        create: (context) => TablecontentCubit(TableRepository())..start(),
-        child: BlocBuilder<TablecontentCubit, TablecontentState>(
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => TablecontentCubit(TableRepository())..start(),
+          ),
+          BlocProvider(
+            create: (context) => ReciptPageCubit(TableRepository())..start(),
+          ),
+        ],
+        child: BlocBuilder<ReciptPageCubit, ReciptPageState>(
           builder: (context, state) {
-            final tablePageModels = state.tablePageModels;
-            return ListView(children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 50, right: 50, top: 20),
-                child: Container(
-                  decoration: const BoxDecoration(color: Colors.orange),
-                  child: Column(
-                    children: [
-                      const Row(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: 8, top: 8, right: 8),
-                            child: Text("NUMER STOLIKA"),
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 15, right: 15, bottom: 8, top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+            final recipts = state.recipts;
+
+            for (final recipt in recipts) {
+              rv1 = recipt.v1;
+              rv2 = recipt.v2;
+              rv3 = recipt.v3;
+              rv4 = recipt.v4;
+              tnumber = recipt.number;
+            }
+            return BlocBuilder<TablecontentCubit, TablecontentState>(
+              builder: (context, state) {
+                final tablePageModels = state.tablePageModels;
+
+                return ListView(children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30, right: 30, top: 20),
+                    child: Container(
+                      decoration: const BoxDecoration(color: Colors.orange),
+                      child: Column(children: [
+                        Row(
                           children: [
-                            Column(
-                              children: [
-                                Container(
-                                  alignment: Alignment.center,
-                                  width: 100,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                    color: Colors.amber,
-                                    border: Border.all(
-                                      width: 2,
-                                      color: Colors.black,
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 8, top: 8, right: 8),
+                              child:
+                                  Text("Table Number : ${tnumber.toString()}"),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 15, right: 15, bottom: 8, top: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                children: [
+                                  Container(
+                                    alignment: Alignment.center,
+                                    width: 100,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      border: Border.all(
+                                        width: 2,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    child: const Text(
+                                      "Drinks",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
                                     ),
                                   ),
-                                  child: const Text(
-                                    "Drinks",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
+                                  const SizedBox(
+                                    height: 8,
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                for (final tablePageModel
-                                    in tablePageModels) ...[
+                                  for (final tablePageModel
+                                      in tablePageModels) ...[
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        width: 100,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                          color: Colors.amber,
+                                          border: Border.all(
+                                            width: 2,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        child: Text(
+                                          tablePageModel.name,
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ),
+                                  ]
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    width: 100,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      border: Border.all(
+                                        width: 2,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    child: const Text(
+                                      "X",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
                                     child: Container(
@@ -93,36 +158,86 @@ class OrdersPage extends StatelessWidget {
                                         ),
                                       ),
                                       child: Text(
-                                        tablePageModel.name,
+                                        rv1.toString(),
                                         style: const TextStyle(
                                             fontWeight: FontWeight.bold),
                                       ),
                                     ),
                                   ),
-                                ]
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      width: 2,
-                                      color: Colors.black,
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    width: 100,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.amber,
+                                      border: Border.all(
+                                        width: 2,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    child: Text(
+                                      rv2.toString(),
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
                                     ),
                                   ),
-                                  child: const Text("ilość"),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                    ],
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    width: 100,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.amber,
+                                      border: Border.all(
+                                        width: 2,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    child: Text(
+                                      rv3.toString(),
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    width: 100,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.amber,
+                                      border: Border.all(
+                                        width: 2,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    child: Text(
+                                      rv4.toString(),
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 24,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ]),
+                    ),
                   ),
-                ),
-              ),
-            ]);
+                ]);
+              },
+            );
           },
         ),
       ),
